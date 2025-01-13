e1f0041e1de87d78fc25fa87dafff6dd939df397
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-01-13 16:03:58 +0100

    Add function to deduplicate mounts

--- e2x_exam_hub/utils.py
+++ e2x_exam_hub/utils.py
@@ -1,47 +1,15 @@
-import os
-
-from traitlets.config import Config
-
-from .config import load_config
+from typing import Dict, List, Union
 
 
-def get_config_file_path(config_mount: str, config_file: str) -> str:
-    """
-    Constructs the full file path for a configuration file.
-
-    Args:
-        config_mount (str): The base directory where the configuration files are mounted.
-        config_file (str): The name of the configuration file.
-
-    Returns:
-        str: The full path to the configuration file.
-    """
-    return os.path.join(config_mount, config_file)
-
-
-def override_singleuser_config(config: Config, path_to_config_file: str) -> None:
-    """
-    Overrides the single-user JupyterHub configuration with settings from a specified configuration
-    file.
-
-    Performs the following operations:
-      - Sets the image for the JupyterHub server.
-      - Sets the image pull policy.
-      - Sets the CPU and memory guarantees and limits for the JupyterHub server.
-
-    Args:
-        config (Config): The JupyterHub configuration object to be modified.
-        path_to_config_file (str): The path to the configuration file containing the new settings.
-    Returns:
-        None
-    """
-
-    server_config = load_config(path_to_config_file)
-
-    config.KubeSpawner.image = server_config.image.full_image_name
-    config.KubeSpawner.image_pull_policy = server_config.image.pull_policy
-
-    config.KubeSpawner.cpu_guarantee = server_config.resources.cpu_guarantee
-    config.KubeSpawner.cpu_limit = server_config.resources.cpu_limit
-    config.KubeSpawner.mem_guarantee = server_config.resources.mem_guarantee
-    config.KubeSpawner.mem_limit = server_config.resources.mem_limit
+def deduplicate_mounts(
+    mounts: List[Dict[str, Union[str, bool]]]
+) -> List[Dict[str, Union[str, bool]]]:
+    existing_mount_paths = set()
+    deduplicated_mounts = []
+    for mount in mounts:
+        if mount["mountPath"] in existing_mount_paths:
+            continue
+        else:
+            existing_mount_paths.add(mount["mountPath"])
+            deduplicated_mounts.append(mount)
+    return deduplicated_mounts
