2b243d654fbc9570b190bea923b6611ab20725fe
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-01-15 14:53:24 +0100

    Add method to get volume mounts in ExamHub

--- e2x_exam_hub/exam_hub.py
+++ e2x_exam_hub/exam_hub.py
@@ -1,10 +1,10 @@
 import os
-from typing import List
+from typing import Dict, List, Union
 
 from traitlets import Unicode
 from traitlets.config import Config, LoggingConfigurable
 
-from .schema import ServerConfig
+from .schema import BaseCourse, ServerConfig
 
 
 class ExamHub(LoggingConfigurable):
@@ -32,18 +32,6 @@ class ExamHub(LoggingConfigurable):
         """
         return os.path.join(self.config_base_path, self.config_file_name)
 
-    def get_hub_users(self) -> List[str]:
-        """
-        Returns a list of all users that are members of the courses configured in the JupyterHub.
-
-        Returns:
-            List[str]: A list of usernames.
-        """
-        users = []
-        for course in self.server_config.nbgrader.student_courses:
-            users.extend(course.course_members)
-        return users
-
     def override_single_user_config(self, config: Config) -> None:
         """
         Overrides the single-user JupyterHub configuration with the image, CPU, and memory settings
@@ -61,3 +49,31 @@ class ExamHub(LoggingConfigurable):
         config.KubeSpawner.cpu_limit = self.server_config.resources.cpu_limit
         config.KubeSpawner.mem_guarantee = self.server_config.resources.mem_guarantee
         config.KubeSpawner.mem_limit = self.server_config.resources.mem_limit
+
+    def get_hub_users(self) -> List[str]:
+        """
+        Returns a list of all users that are members of the courses configured in the JupyterHub.
+
+        Returns:
+            List[str]: A list of usernames.
+        """
+        users = []
+        for course in self.server_config.nbgrader.student_courses:
+            users.extend(course.course_members)
+        return users
+
+    def get_volume_mounts(
+        self, course: BaseCourse, username: str
+    ) -> List[Dict[str, Union[str, bool]]]:
+        """
+        Retrieve the volume mounts for a given course and username.
+
+        Args:
+            course (BaseCourse): The course for which to retrieve volume mounts.
+            username (str): The username of the user for whom to retrieve volume mounts.
+
+        Returns:
+            List[Dict[str, Union[str, bool]]]: A list of dictionaries containing volume mount
+            information.
+        """
+        return self.server_config.mounts.get_mounts(course, username)
