a896ad10e131db466dd3b339124a15c1200793e4
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-01-16 13:30:06 +0100

    Format utils.py

--- e2x_exam_hub/utils.py
+++ e2x_exam_hub/utils.py
@@ -4,12 +4,12 @@ from typing import Dict, List, Union
 def deduplicate_mounts(
     mounts: List[Dict[str, Union[str, bool]]]
 ) -> List[Dict[str, Union[str, bool]]]:
-    existing_mountPaths = set()
+    existing_mount_paths = set()
     deduplicated_mounts = []
     for mount in mounts:
-        if mount["mountPath"] in existing_mountPaths:
+        if mount["mountPath"] in existing_mount_paths:
             continue
         else:
-            existing_mountPaths.add(mount["mountPath"])
+            existing_mount_paths.add(mount["mountPath"])
             deduplicated_mounts.append(mount)
     return deduplicated_mounts
